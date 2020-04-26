f8124a94f084f408a152d9591d8f757c866b6acd
Boris <abirme@yandex.ru>
2020-04-26 19:25:55 +0300

    по мелочи...

--- src/Warehouse/utils/Count.java
+++ src/Warehouse/utils/Count.java
@@ -3,6 +3,9 @@ package Warehouse.utils;
 //подсчет времени
 public class Count {
     public static void main(String[] args) {
+
+
+
         long start = System.currentTimeMillis();
         //вычисление
         for (int i = 0; i <10000000 ; i++) {
