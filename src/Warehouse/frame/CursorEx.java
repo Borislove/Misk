89beca60f9a4b4d871dd9963f2db5928a6d710c5
Borislove <abirme@yandex.ru>
2021-03-15 14:54:30 +0300

    cursor

--- src/Warehouse/frame/CursorEx.java
+++ src/Warehouse/frame/CursorEx.java
@@ -1,13 +1,21 @@
 package Warehouse.frame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class CursorEx {
 
     JFrame frame;
 
-    public CursorEx() {
+    //BufferedImage bufferedImage = new BufferedImage(200,200, BufferedImage.TYPE_INT_RGB); //квадратик
+    BufferedImage image = ImageIO.read(new File("e:\\javadoc\\cursor_5.png"));
+
+
+    public CursorEx() throws IOException {
         frame = new JFrame();
         //  Toolkit toolkit = Toolkit.getDefaultToolkit();
         //   Image image = toolkit.getImage("E:\\javadoc\\утка.png");
@@ -15,15 +23,23 @@ public class CursorEx {
         frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
 
+        /*frame.setCursor((Toolkit.getDefaultToolkit().createCustomCursor(
+                //   new ImageIcon("E:\\javadoc\\test.png").getImage(),
+                //            new ImageIcon("E:\\javadoc\\finger.jpg").getImage(),
+                new ImageIcon(image).getImage(),
+                new Point(0, 0), "custom cursor")));*/
+
         frame.setCursor((Toolkit.getDefaultToolkit().createCustomCursor(
-                new ImageIcon("E:\\javadoc\\test.png").getImage(),
+                //   new ImageIcon("E:\\javadoc\\test.png").getImage(),
+                //            new ImageIcon("E:\\javadoc\\finger.jpg").getImage(),
+                image,
                 new Point(0, 0), "custom cursor")));
 
         frame.setVisible(true);
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new CursorEx();
     }
 }
